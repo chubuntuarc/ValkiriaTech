bf68601d6586746c0715e15d9468ed8233411a2e
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-08-21 21:34:18 -0600

    Perfil de usuario layout basico

--- tech/front/side_menu.php
+++ tech/front/side_menu.php
@@ -3,7 +3,7 @@
            <li id="user_img_side" class="center-align">
                <img src="https://avatars2.githubusercontent.com/u/9604554?v=3&s=460" alt="" class="circle responsive-img" style="height:150px;width:150px;">
            </li>
-           <li><a href="#">Jesus Arciniega</a></li>
+           <li><a href="profile.php">Jesus Arciniega</a></li>
            <li><a href="warehouse.php">Almacén</a></li>
            <li><a href="products.php">Productos</a></li>
            <li><a href="categories.php">Categorías</a></li>
