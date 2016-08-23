4a1520773ed391554bff7016f9d53d015142227d
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-08-22 21:23:19 -0600

    Menos variables sin uso

--- tech/back/session.php
+++ tech/back/session.php
@@ -1,8 +1,4 @@
 <?php
 /*session is started if you don't write this line can't use $_Session  global variable*/
-$_SESSION["module_id"];
-$_SESSION["module"];
-$_SESSION["module_description"];
-$_SESSION["new_element"];
 include_once("back/module_data.php");
 ?>
